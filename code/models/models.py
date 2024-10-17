4f16f5ae06716543cc95c810533be0da2e8c94fd
AgamChopra <agamacdc@gmail.com>
2024-10-17 16:26:14 -0500

    models and training updated

--- code/models/models.py
+++ code/models/models.py
@@ -1,153 +1,149 @@
+import torch
 import torch.nn as nn
-import ViT as vit
+import torch.nn.functional as F
 
 
-#Critic    
-class CriticA(nn.Module): # (N,3,128,128) -> (N,1)
-    def __init__(self,CH):
-        super(CriticA, self).__init__()
-        c1,c2,c3,c4,c5 = 64, 128, 256, 512, 1024
-        self.l1 = nn.Sequential(nn.utils.spectral_norm(nn.Conv2d(CH, c1, 4, 2, padding = 1)), nn.LeakyReLU(0.2,inplace=False),
-                                nn.utils.spectral_norm(nn.Conv2d(c1, c2, 4, 2, padding = 1)), nn.LeakyReLU(0.2,inplace=False),
-                                nn.utils.spectral_norm(nn.Conv2d(c2, c3, 4, 2, padding = 1)), nn.LeakyReLU(0.2,inplace=False),
-                                nn.utils.spectral_norm(nn.Conv2d(c3, c4, 4, 2, padding = 1)), nn.LeakyReLU(0.2,inplace=False),
-                                nn.utils.spectral_norm(nn.Conv2d(c4, c5, 4, 2, padding = 1)), nn.LeakyReLU(0.2,inplace=False))
-        self.l2 = nn.Conv2d(c5, 1, 4, 1)
-        
-    def forward(self, x):
-        y = self.l1(x)
-        y = self.l2(y).squeeze()
-        return  y   
-    
-    
-class CriticB(nn.Module): # (N,3,128,128) -> (N,1)
-    def __init__(self,CH):
-        super(CriticB, self).__init__()
-        self.c = 40
-        
-        self.E = nn.Sequential(nn.utils.spectral_norm(nn.Conv2d(CH, self.c, kernel_size=3, stride=1)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.utils.spectral_norm(nn.Conv2d(self.c, self.c, kernel_size=3, stride=2)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.utils.spectral_norm(nn.Conv2d(self.c, self.c, kernel_size=3, stride=1)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.utils.spectral_norm(nn.Conv2d(self.c, self.c, kernel_size=3, stride=2)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.utils.spectral_norm(nn.Conv2d(self.c, self.c, kernel_size=3, stride=1)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.utils.spectral_norm(nn.Conv2d(self.c, self.c, kernel_size=3, stride=2)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.utils.spectral_norm(nn.Conv2d(self.c, self.c, kernel_size=3, stride=1)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.utils.spectral_norm(nn.Conv2d(self.c, self.c, kernel_size=3, stride=1)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.utils.spectral_norm(nn.Conv2d(self.c, self.c, kernel_size=3, stride=2)), nn.LeakyReLU(0.2,inplace=False),
-                               nn.MaxPool2d(kernel_size=4, padding=0, stride=4))     
-        
-        self.fc = nn.Sequential(nn.Linear(self.c, 1))
-        
-    def forward(self, x):
-        y = self.E(x)
-        y = self.fc(y.squeeze())
-        return  y    
-    
-    
-class CriticC(nn.Module): # (N,3,128,128) -> (N,1)
-    def __init__(self,CH):
-        super(CriticC, self).__init__()
-        self.l1 = nn.Sequential(nn.utils.spectral_norm(nn.Conv2d(CH, 64, 4, 2, padding = 1)), nn.LeakyReLU(0.2,inplace=False))
-        self.l2 = vit.VisionTransformer(img_size = 64, patch_size = 16, in_c = 64, n_classes = 1, depth = 5)
-        
-    def forward(self, x):
-        y = self.l1(x)
-        y = self.l2(y).squeeze()
-        return  y 
-         
-    
-def Critic(mode='A',CH=3):
-    if mode == 'A':
-        model = CriticA(CH)
-    elif mode == 'B':
-        model = CriticB(CH)
-    elif mode == 'C':
-        model = CriticC(CH)
-    else:
-        model = None
-    return model    
+class Critic(nn.Module):  # (N,3,64,64) -> (N,1)
+    def __init__(self, input_channels, num_feature_maps=256):
+        super(Critic, self).__init__()
+        self.c = num_feature_maps
+
+        self.feature_extractor = nn.Sequential(
+            MultiKernelConv2d(input_channels, self.c, stride=2,
+                              padding=1, apply_spectral_norm=True,
+                              groups=1),
+            nn.Mish(),
+            MultiKernelConv2d(self.c, self.c, stride=2,
+                              padding=1, apply_spectral_norm=True,
+                              groups=2),
+            nn.Mish(),
+            MultiKernelConv2d(self.c, self.c * 2, stride=2,
+                              padding=1, apply_spectral_norm=True,
+                              groups=4),
+            nn.Mish(),
+            MultiKernelConv2d(self.c * 2, self.c * 4, stride=2,
+                              padding=1, apply_spectral_norm=True,
+                              groups=4),
+            nn.Mish(),
+            MultiKernelConv2d(self.c * 4, self.c * 8, stride=2,
+                              padding=1, apply_spectral_norm=True,
+                              groups=1),
+            nn.Mish(),
+            nn.AdaptiveAvgPool2d(1)
+        )
+
+        self.fully_connected = nn.Sequential(nn.Linear(self.c * 8, 1))
+
+    def forward(self, input_tensor):
+        features = self.feature_extractor(input_tensor)
+        features = self.fully_connected(features.squeeze())
+        return features
 
 
-#Generator. Returns floating point RGB (3 channel with continous range of [0.,255.]) images.
-class GeneratorB(nn.Module): # 128 -> 29 -> 128
-    def __init__(self,CH):
-        super(GeneratorB, self).__init__()
-        c1,c2,c3,c4,c5 = 1024, 512, 256, 128, 64
-        self.E = nn.Sequential(nn.ConvTranspose2d(100, c1, 4, 1), nn.Tanh(),
-                               nn.ConvTranspose2d(c1, c2, 4, 2, padding = 1), nn.Tanh(),
-                               nn.ConvTranspose2d(c2, c3, 4, 2, padding = 1), nn.Tanh(),
-                               nn.ConvTranspose2d(c3, c4, 4, 2, padding = 1), nn.Tanh(),
-                               nn.ConvTranspose2d(c4, c5, 4, 2, padding = 1), nn.Tanh(),
-                               nn.ConvTranspose2d(c5, CH, 4, 2, padding = 1), nn.Tanh())        
-    
-    def forward(self, x):
-        y = self.E(x)
-        output = (y + 1)/2 #((y - y.min())/(y.max() - y.min()))
-        return  output
-    
-    
-class GeneratorA(nn.Module): # (N,100,1,1) -> (N,3,128,128)
-    def __init__(self,CH):
-        super(GeneratorA, self).__init__()
-        self.a,self.c_,self.c = 16,24,40
-        
-        self.fc = nn.Sequential(nn.Linear(100, self.c_ * self.a * self.a), nn.ReLU(inplace=False), nn.BatchNorm1d(self.c_ * self.a * self.a))
-        
-        self.E = nn.Sequential(nn.ConvTranspose2d(self.c_, self.c, kernel_size=4, padding=1, stride=2), nn.ReLU(inplace=False), nn.BatchNorm2d(self.c),
-                               nn.Conv2d(self.c, self.c, kernel_size=3, padding=1, stride=1), nn.ReLU(inplace=False), nn.BatchNorm2d(self.c),
-                               nn.Conv2d(self.c, self.c, kernel_size=3, padding=1, stride=1), nn.ReLU(inplace=False), nn.BatchNorm2d(self.c),
-                               nn.Conv2d(self.c, self.c, kernel_size=3, padding=1, stride=1), nn.ReLU(inplace=False), nn.BatchNorm2d(self.c),
-                               nn.ConvTranspose2d(self.c, self.c, kernel_size=4, padding=1, stride=2), nn.ReLU(inplace=False), nn.BatchNorm2d(self.c),
-                               nn.Conv2d(self.c, self.c, kernel_size=3, padding=1, stride=1), nn.ReLU(inplace=False), nn.BatchNorm2d(self.c),
-                               nn.ConvTranspose2d(self.c, self.c, kernel_size=4, padding=1, stride=2), nn.ReLU(inplace=False), nn.BatchNorm2d(self.c),
-                               nn.Conv2d(self.c, CH, kernel_size=3, padding=1, stride=1), nn.Tanh())        
-    
-    def forward(self, x):
-        y = self.fc(x.view(x.shape[0],100))
-        y = self.E(y.view(x.shape[0],self.c_,self.a,self.a))
-        output = (y + 1)/2
-        return  output
-    
-    
-class GeneratorC(nn.Module):
-    def __init__(self,CH):
-        super(GeneratorC, self).__init__()
-        c1,c2,c3 = 512, 128, 32
-        self.F = nn.Sequential(nn.ConvTranspose2d(1, c1, 4, 3), nn.Tanh(),
-                                nn.ConvTranspose2d(c1, c2, 3, 2), nn.Tanh(),
-                                nn.ConvTranspose2d(c2, c3, 3, 2), nn.Tanh(),
-                                nn.ConvTranspose2d(c3, CH, 2, 1), nn.Sigmoid())
+class Generator(nn.Module):  # (N,noise_dim,1,1) -> (N,3,64,64)
+    def __init__(self, input_channels, noise_dim=100, initial_spatial_size=4,
+                 num_initial_feature_maps=256, num_feature_maps=256):
+        super(Generator, self).__init__()
+        self.a = initial_spatial_size
+        self.c_ = num_initial_feature_maps
+        self.c = num_feature_maps
 
-    def forward(self, x):
-        output = self.F(x)
-        return  output
-    
-    
-def Generator(mode='A',CH=3):
-    if mode == 'A':
-        model = GeneratorA(CH)
-    elif mode == 'B':
-        model = GeneratorB(CH)
-    elif mode == 'C':
-        model = GeneratorC(CH)
-    else:
-        model = None
-    return model
+        self.fully_connected = nn.Sequential(
+            nn.Linear(noise_dim, self.c_ * self.a * self.a),
+            nn.Mish(),
+            nn.BatchNorm1d(self.c_ * self.a * self.a)
+        )
+
+        self.feature_extractor = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+            MultiKernelConv2d(self.c_, self.c),
+            nn.Mish(),
+            nn.BatchNorm2d(self.c),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+            MultiKernelConv2d(self.c, self.c),
+            nn.Mish(),
+            nn.BatchNorm2d(self.c),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+            MultiKernelConv2d(self.c, self.c),
+            nn.Mish(),
+            nn.BatchNorm2d(self.c),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+            MultiKernelConv2d(self.c, self.c),
+            nn.Mish(),
+            nn.BatchNorm2d(self.c),
+            nn.Conv2d(self.c, input_channels, kernel_size=1),
+            nn.Tanh()
+        )
+
+    def forward(self, input_tensor):
+        features = self.fully_connected(
+            input_tensor.view(input_tensor.shape[0], 100))
+        features = self.feature_extractor(features.view(
+            input_tensor.shape[0], self.c_, self.a, self.a))
+        output = (features + 1) / 2
+        return output
 
 
-# =============================================================================
-# import torch  
-# torch.manual_seed(0)
-# x = torch.ones(64,3,128,128).cuda()
-# m = CriticB(3).cuda()
-# y = m(x)
-# print(y.shape,y.mean()) 
-# =============================================================================
-# =============================================================================
-# import torch  
-# torch.manual_seed(0)
-# x = torch.rand((64,1,10,10)).cuda()
-# m = Generator('A').cuda()
-# y = m(x)
-# print(y.shape)
-# =============================================================================
+class MultiKernelConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_sizes=[1, 3, 5, 7],
+                 stride=1, padding=0, groups=1, apply_spectral_norm=False):
+        super(MultiKernelConv2d, self).__init__()
+        self.convs = nn.ModuleList([
+            nn.Conv2d(in_channels, out_channels // len(kernel_sizes),
+                      kernel_size=k, stride=stride, padding=k // 2,
+                      groups=groups)
+            for k in kernel_sizes
+        ])
+        if apply_spectral_norm:
+            self.convs = nn.ModuleList(
+                [nn.utils.spectral_norm(conv) for conv in self.convs])
+        self.padding = padding
+
+    def forward(self, input_tensor):
+        out = torch.cat([conv(input_tensor) for conv in self.convs], dim=1)
+        if self.padding > 0:
+            out = F.pad(out, (self.padding, self.padding,
+                        self.padding, self.padding))
+        return out
+
+
+def main():
+    print('cuda detected:', torch.cuda.is_available())
+    torch.set_printoptions(precision=9)
+
+    # 'highest', 'high', 'medium'. 'highest' is slower but accurate while 'medium'
+    #  is faster but less accurate. 'high' is preferred setting. Refer:
+    #  https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html
+    torch.set_float32_matmul_precision('high')
+
+    # 'True' = faster but less accurate, 'False' = Slower but more accurate
+    #  has to be set to True if presision is high or medium
+    torch.backends.cuda.matmul.allow_tf32 = True
+
+    # 'True' = faster but less accurate, 'False' = Slower but more accurate
+    #  has to be set to True if presision is high or medium
+    torch.backends.cudnn.allow_tf32 = True
+
+    # For stability 'False', 'True', might be slower
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = False
+
+    # Define input tensors
+    input_critic = torch.randn(64, 3, 64, 64).cuda()
+    input_generator = torch.randn(64, 100, 1, 1).cuda()
+
+    # Initialize models
+    critic = Critic(input_channels=3).cuda()
+    generator = Generator(input_channels=3).cuda()
+
+    # Test Generator forward pass
+    generator_output = generator(input_generator)
+    print("Generator Output shape:", generator_output.shape)
+
+    # Test Critic forward pass
+    critic_output = critic(input_critic)
+    print("Critic Output:", critic_output.shape)
+
+
+if __name__ == "__main__":
+    main()
--- code/train.py
+++ code/train.py
@@ -1,10 +1,29 @@
 import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 from tqdm import trange
 import models
 import dataset as dst
 
+print('cuda detected:', torch.cuda.is_available())
+torch.set_printoptions(precision=9)
 
-print('cuda detected:',torch.cuda.is_available())
+# 'highest', 'high', 'medium'. 'highest' is slower but accurate while 'medium'
+#  is faster but less accurate. 'high' is preferred setting. Refer:
+#  https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html
+torch.set_float32_matmul_precision('highest')
+
+# 'True' = faster but less accurate, 'False' = Slower but more accurate
+#  has to be set to True if precision is high or medium
+torch.backends.cuda.matmul.allow_tf32 = False
+
+# 'True' = faster but less accurate, 'False' = Slower but more accurate
+#  has to be set to True if precision is high or medium
+torch.backends.cudnn.allow_tf32 = False
+
+# For stability 'False', 'True', might be slower
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
 
 
 def save_checkpoint_log(list_):
@@ -18,130 +37,145 @@ def save_checkpoint_log(list_):
 
 def grad_penalty(critic, real, fake, weight):
     b_size, c, h, w = real.shape
-    epsilon = torch.rand(b_size, 1, 1, 1).repeat(1,c,h,w).cuda()
+    epsilon = torch.rand(b_size, 1, 1, 1).repeat(1, c, h, w).cuda()
     interp_img = (real * epsilon) + (fake * (1 - epsilon))
-    
+
     mix_score = critic(interp_img)
-    
-    grad = torch.autograd.grad(outputs = mix_score, 
-                                   inputs = interp_img, 
-                                   grad_outputs=torch.ones_like(mix_score).cuda(),
-                                   create_graph=True,
-                                   retain_graph=True)[0]
-    
-    grad = grad.view(b_size,-1)
-    #Calculating manual gradient norm to avoid issues around 0 by adding epsilon = 1E-12. {grad.norm(2,dim=1)}
+
+    grad = torch.autograd.grad(outputs=mix_score,
+                               inputs=interp_img,
+                               grad_outputs=torch.ones_like(mix_score).cuda(),
+                               create_graph=True,
+                               retain_graph=True)[0]
+
+    grad = grad.view(b_size, -1)
+    # Calculating manual gradient norm to avoid issues around 0 by adding epsilon = 1E-12. {grad.norm(2,dim=1)}
     grad_norm = torch.sqrt((torch.sum(grad ** 2, dim=1)) + 1E-12)
     penalty = torch.mean((grad_norm - 1) ** 2)
     return weight * penalty
 
 
-def advers_train(dataset, lr = 1E-4, epochs = 5, batch=32, beta1=0.5, beta2=0.999, critic_iter=5, Lambda_penalty = 10, dmt = 32, load_state = False,gmod='A',cmod='A',state=None):
-    
+def advers_train(dataloader, lr=1E-4, epochs=5, batch=32, beta1=0.5, beta2=0.999,
+                 critic_iter=5, Lambda_penalty=10, dmt=32, load_state=False,
+                 state=None):
+
     REG = int(batch / dmt)
-    batch -=  REG    
-    
+    batch -= REG
+
     Closses = []
     Glosses = []
-    
-    CH = dataset.shape[1]
-    
-    print('loading generator...', end =" ")
-    #Generator 
-    Gen = models.Generator(gmod,CH).cuda()
+
+    CH = 3
+
+    print('loading generator...', end=" ")
+    # Generator
+    Gen = models.Generator(CH).cuda()
     print('done.')
-    
-    print('loading critic...', end =" ")
-    #Critic
-    Crit = models.Critic(cmod,CH).cuda()   
+
+    print('loading critic...', end=" ")
+    # Critic
+    Crit = models.Critic(CH).cuda()
     print('done.')
-    
+
     if load_state:
-        print('loading previous run state...', end =" ")
-        Gen.load_state_dict(torch.load("Gen-Autosave.pt"))
-        Crit.load_state_dict(torch.load("Crit-Autosave.pt"))  
+        print('loading previous run state...', end=" ")
+        Gen.load_state_dict(torch.load("/home/agam/Documents/git_projects/Gen-Autosave.pt"))
+        Crit.load_state_dict(torch.load("/home/agam/Documents/git_projects/Crit-Autosave.pt"))
         print('done.')
-        
+
     if state is not None:
         Gen.load_state_dict(torch.load(state[0]))
-        Crit.load_state_dict(torch.load(state[1]))  
-    
-    optimizerG = torch.optim.Adam(Gen.parameters(),lr,betas=(beta1, beta2))
-    optimizerC = torch.optim.Adam(Crit.parameters(),lr,betas=(beta1, beta2))
-    
+        Crit.load_state_dict(torch.load(state[1]))
+
+    optimizerG = torch.optim.Adam(Gen.parameters(), lr, betas=(beta1, beta2))
+    optimizerC = torch.optim.Adam(Crit.parameters(), lr, betas=(beta1, beta2))
+
     print('optimizing...')
+    print(dataloader.batch_size, len(dataloader.dataset))
+
     for eps in trange(epochs):
-        
-        idx_ = torch.randperm(dataset.shape[0])
         ctr = 1
-        
-        for b in range(0,dataset.shape[0]-batch,batch):            
-            
-            ###Critic###            
-            optimizerC.zero_grad() 
-            real = dataset[idx_[b:b+batch]].cuda() / 255.
-            
-            x = torch.rand((REG,1,10,10)).cuda()
+
+        for i, (real, _) in enumerate(dataloader):
+            if i * dataloader.batch_size >= len(dataloader.dataset):
+                break
+
+            real = real.cuda()
+            optimizerC.zero_grad()
+
+            x = torch.rand((REG, 100, 1, 1)).cuda()
             fake = Gen(x)
-            real = torch.cat((real,fake),dim=0) # Adding noise to real data to improve generator stability and weaken critic
-            
-            x = torch.rand((real.shape[0],1,10,10)).cuda()
-            fake = Gen(x) 
-                            
-            #Real
+            # Adding noise to real data to improve generator stability and weaken critic
+            real = torch.cat((real, fake), dim=0)
+
+            x = torch.rand((real.shape[0], 100, 1, 1)).cuda()
+            fake = Gen(x)
+
+            # Real
             y = Crit(real)
             errC_real = y.mean()
-                
-            #fake
+
+            # Fake
             y = Crit(fake.detach())
             errC_fake = y.mean()
-                
-            #total err/loss
-            penalty = grad_penalty(Crit, real, fake, Lambda_penalty) 
-            errC = errC_fake - errC_real + penalty                   
-            errC.backward(retain_graph = True)
-            optimizerC.step()         
-            
+
+            # Total err/loss
+            penalty = grad_penalty(Crit, real, fake, Lambda_penalty)
+            errC = errC_fake - errC_real + penalty
+            errC.backward(retain_graph=True)
+            optimizerC.step()
+
             if ctr % critic_iter == 0:
-                ###Generator###
-                x = torch.rand((real.shape[0],1,10,10)).cuda()
+                ### Generator###
+                x = torch.rand((real.shape[0], 100, 1, 1)).cuda()
                 fake = Gen(x)
-                
-                optimizerG.zero_grad()            
+
+                optimizerG.zero_grad()
                 y = Crit(fake)
                 errG = -y.mean()
-                errG.backward()            
+                errG.backward()
                 optimizerG.step()
-                
-                #Misc.
+
+                # Misc.
                 Closses.append(errC.item())
                 Glosses.append(errG.item())
-                
+
                 if ctr % (critic_iter * 100) == 0:
-                    print('[%d/%d][%d/%d]\tLoss_C: %.4f\tLoss_G: %.4f'% (eps, epochs, int(b/batch), int(len(idx_)/batch), errC.item(), errG.item()))  
-                    dst.visualize_16(fake[:16].cpu().detach().numpy(),dark = True) #dst.visualize(fake[0].cpu().detach().numpy())
-                    torch.save(Gen.state_dict(), "Gen-Autosave.pt")
-                    torch.save(Crit.state_dict(), "Crit-Autosave.pt")
-                    
+                    print('[%d/%d][%d/%d]\tLoss_C: %.4f\tLoss_G: %.4f' % (eps, epochs,
+                          ctr, len(dataloader), errC.item(), errG.item()))
+                    dst.visualize_16(
+                        fake[:16].cpu().detach().numpy(), dark=True)
+
+            if eps % 10 == 0:
+                torch.save(Gen.state_dict(), "/home/agam/Documents/git_projects/Gen-Autosave.pt")
+                torch.save(Crit.state_dict(), "/home/agam/Documents/git_projects/Crit-Autosave.pt")
+
             ctr += 1
-                 
-      
-    return Gen, Crit, Closses, Glosses#, checkpoint_log
+
+    return Gen, Crit, Closses, Glosses
 
 
-def train(gmod = 'A',cmod = 'B', Gsave = "\Gen_temp.pt", Csave = "Crit_temp.pt"):
-    
+def train():
     print('loading data...')
-    dataset = dst.torch_celeb_dataset()
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+    ])
+    celeb_dataset = datasets.CelebA(
+        root='./data', split='train', download=True, transform=transform)
+    dataloader = DataLoader(celeb_dataset, batch_size=64, shuffle=True)
     print('done.')
 
-    EPOCHS = 50
-    BATCH = 64
-    print("Gen-Autosave.pt")
-    print("Crit-Autosave.pt")
-    Gen,Crit,Dl,Gl = advers_train(dataset=dataset,lr=1E-4,epochs=EPOCHS,batch=BATCH,dmt=BATCH/2,critic_iter=5,load_state = False,gmod=gmod,cmod=cmod)#,state=[Gsave,Csave])
-   
-    dst.plt.figure(figsize=(10,5))
+    EPOCHS = 1000
+    BATCH = 128
+    print("/home/agam/Documents/git_projects/Gen-Autosave.pt")
+    print("/home/agam/Documents/git_projects/Crit-Autosave.pt")
+    Gen, Crit, Dl, Gl = advers_train(
+        dataloader=dataloader, lr=1E-4, epochs=EPOCHS, batch=BATCH,
+        dmt=BATCH/2, critic_iter=5, load_state=False
+    )
+
+    dst.plt.figure(figsize=(10, 5))
     dst.plt.title("Generator and Critic Loss During Training")
     dst.plt.plot(Dl, label='D_loss')
     dst.plt.plot(Gl, label='G_loss')
@@ -150,39 +184,38 @@ def train(gmod = 'A',cmod = 'B', Gsave = "\Gen_temp.pt", Csave = "Crit_temp.pt")
     dst.plt.ylabel("Loss")
     dst.plt.legend()
     dst.plt.show()
-    
-    torch.save(Gen.state_dict(), Gsave)
-    torch.save(Crit.state_dict(), Csave)
-    
+
+    torch.save(Gen.state_dict(), "/home/agam/Documents/git_projects/Gen-Autosave.pt")
+    torch.save(Crit.state_dict(), "/home/agam/Documents/git_projects/Crit-Autosave.pt")
+
     return Gen
 
 
-def gen_img(Gen = None, gmod = 'A'):
+def gen_img(Gen=None):
     if Gen is None:
-        Gsave = "Gen_temp.pt"
-        Gen = models.Generator(gmod,3).cuda()
+        Gsave = "/home/agam/Documents/git_projects/Gen_temp.pt"
+        Gen = models.Generator(3).cuda()
         try:
             Gen.load_state_dict(torch.load(Gsave))
-        except:
-            print('Warning: Could not load generator parameters at',Gsave)
+        except Exception:
+            print('Warning: Could not load generator parameters at', Gsave)
     for _ in range(20):
-        noise = torch.rand((25,1,10,10)).cuda()
+        noise = torch.rand((25, 1, 10, 10)).cuda()
         warped = Gen(noise)
         wd = warped.cpu().detach().numpy()
         print(warped.shape)
-        dst.visualize_25(wd,dark=False)
-    
-    
+        dst.visualize_25(wd, dark=False)
+
+
 def main():
-    x = int(input('Would you like to train model(press \'1\') or generate synthetic images from previous state(press \'2\')?'))
-    #x=1
+    x = int(input("Would you like to train model(press '1') or generate synthetic images from previous state(press '2')?"))
     if x == 1:
         train()
-    elif x==2:
+    elif x == 2:
         gen_img()
     else:
         print('Value Error: Please enter either 1 or 2')
-        
+
 
 if __name__ == "__main__":
     main()
